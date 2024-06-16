d566da269110b67c444407be1ee4dacd777c932c
hellooomoerta <hilding_1989@hotmail.com>
2024-06-16 15:06:53 +0200

    Custom Property Attribute Drawer suck och fittigt stön...

---
+++ Clonedew Valley/Assets/Utility/Property Drawers/ItemCodeDescriptionAttribute.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public class ItemCodeDescriptionAttribute : PropertyAttribute
+{
+    // No values need to be held for the item code description attribute
+    // So this class can be empty
+}
